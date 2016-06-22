dec4ea96c0a3f0ca7bde18acc6f5916382d75389
Pavan Podila <pavan.podila@gmail.com>
2016-06-22 14:43:49 -0400

    decorators

--- src/app/components/clock/clock.jsx
+++ src/app/components/clock/clock.jsx
@@ -6,30 +6,13 @@ import {connect} from 'mobx-connect';
 export default class Clock extends React.Component {
 
     render() {
-        const {store: {date}} = this.context;
+        const {store: {time}} = this.context;
         return (
             <div>
                 <span>The time is</span>
-                <h2>{format(date)}</h2>
+                <h2>{time}</h2>
             </div>
         );
     }
-
-    componentWillMount() {
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-    }
 }
 
-function format(date) {
-    const yyyy = date.getFullYear(),
-        mm = date.getMonth() + 1,
-        dd = date.getDate(),
-        hh = date.getHours(),
-        m = date.getMinutes(),
-        s = date.getSeconds();
-
-    return `${mm}/${dd}/${yyyy} ${hh}:${m}:${s}`;
-}
--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,11 @@
-import {observable} from 'mobx';
+import {computed, observable} from 'mobx';
 
 class Store {
     @observable date = new Date();
+
+    @computed get time() {
+        return format(this.date);
+    }
 
     init() {
         this.intervalId = setInterval(()=> this.tick(), 1000);
@@ -16,3 +20,14 @@ const store = new Store();
 store.init();
 
 export {store};
+
+function format(date) {
+    const yyyy = date.getFullYear(),
+        mm = date.getMonth() + 1,
+        dd = date.getDate(),
+        hh = date.getHours(),
+        m = date.getMinutes(),
+        s = date.getSeconds();
+
+    return `${mm}/${dd}/${yyyy} ${hh}:${m}:${s}`;
+}
