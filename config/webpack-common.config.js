014f072958fabecf1a138fcb7c19bcba25da4e95
pavanpodila <pavan.podila@gmail.com>
2016-06-25 18:06:39 -0400

    cleaning up

--- config/webpack-common.config.js
+++ config/webpack-common.config.js
@@ -46,10 +46,6 @@ module.exports = {
 
             {
                 // Do not transform vendor's CSS with CSS-modules
-                // The point is that they remain in global scope.
-                // Since we require these CSS files in our JS or CSS files,
-                // they will be a part of our compilation either way.
-                // So, no need for ExtractTextPlugin here.
                 test: /\.css$/,
                 include: /node_modules/,
                 loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
