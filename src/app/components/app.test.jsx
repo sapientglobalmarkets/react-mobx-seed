3c96292349cf0a91584b2ba20d1870d142fb1084
nareshbhatia <naresh.archfirst@gmail.com>
2016-07-02 00:17:42 -0400

    Fixed import in app.test.jsx

--- src/app/components/app.test.jsx
+++ src/app/components/app.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import { App } from './';
 import {Provider} from 'mobx-react';
+import App from './app';
 
 describe('<App />', ()=> {
     beforeEach(()=> {
--- src/clock/components/clock-page.test.jsx
+++ src/clock/components/clock-page.test.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import ClockPage from './clock-page';
 import {Provider} from 'mobx-react';
+import ClockPage from './clock-page';
 
-describe('<Clock>', ()=> {
+describe('<Clock>', () => {
 
-    beforeEach(()=> {
+    beforeEach(() => {
         this.store = { clock: {date: new Date()}};
     });
 
-    it('should render correctly', ()=> {
+    it('should render correctly', () => {
         const dom = shallow(
             <Provider store={this.store}>
                 <ClockPage/>
@@ -18,14 +18,13 @@ describe('<Clock>', ()=> {
         expect(dom).to.have.length(1);
     });
 
-    it('should render the time in <h2>', ()=> {
+    it('should render the time in <h2>', () => {
         const dom = mount(
             <Provider store={this.store}>
                 <ClockPage/>
             </Provider>
         );
         expect(dom.find('h2').length).to.be.greaterThan(0);
-
     });
 
 });
