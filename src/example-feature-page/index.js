742f7a7032f0cb860142bdee226d9f909a6f6f11
Jeff Friesen <momoblackblack@gmail.com>
2016-10-14 13:14:55 -0600

    Removing example to get ready for PR

--- src/example-feature-page/index.js
+++
@@ -1,2 +0,0 @@
-export ExampleFeaturePage from './components/example-feature-page';
-export exampleFeaturePageStore from './example-feature-page.store';
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,13 +3,11 @@ import {Route, IndexRoute, Redirect} from 'react-router';
 import {Shell} from './shell';
 import {ClockPage} from './clock';
 import {GithubPage} from './github';
-import {ExampleFeaturePage} from './example-feature-page';
 
 export let routes = (
     <Route path="/" component={Shell}>
         <IndexRoute component={GithubPage}/>
         <Route path="clock" component={ClockPage}/>
-        <Route path="example-feature-page" component={ExampleFeaturePage}/>
         <Redirect from="*" to="/"/>
     </Route>
 );
